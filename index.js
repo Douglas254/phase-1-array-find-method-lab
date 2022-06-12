ad2aa92221bbf9dac87b2db739561436ef88fef2
Douglas Obara <douglasobara97@gmail.com>
2022-06-12 20:01:21 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+/*
+    returns a year the Denver Broncos won the superbowl
+    returns undefined if the record has no win objects
+*/
+const superbowlWin = (record) => {
+  let WinYear;
+  for (let i = 0; i < record.length; i++) {
+    const data = record[i];
+    if (data.result == "W") {
+      WinYear = data.year;
+      break;
+    }
+  }
+  return WinYear;
+};
